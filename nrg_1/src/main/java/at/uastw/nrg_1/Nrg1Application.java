282d711c7bb2e5b76bb3f8fceab5168373167c5a
Patrick Kirchknopf <pat.kir.1711@gmail.com>
2025-05-03 18:45:36 +0200

    hehexD

---
+++ nrg_1/src/main/java/at/uastw/nrg_1/Nrg1Application.java
@@ -0,0 +1,13 @@
+package at.uastw.nrg_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nrg1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nrg1Application.class, args);
+	}
+
+}
