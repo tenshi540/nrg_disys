7ca4d5fee5e1987ad2f324047d6f2d1133ed47e7
Patrick Kirchknopf <pat.kir.1711@gmail.com>
2025-05-03 19:51:46 +0200

    XDDDDDDDD

--- nrg_1/src/main/java/at/uastw/nrg_1/controller/EnergyController.java
+++ nrg_1/src/main/java/at/uastw/nrg_1/controller/EnergyController.java
@@ -1,7 +1,23 @@
 package at.uastw.nrg_1.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+//xD
+
+@RestController
+@RequestMapping("/energy")
 public class EnergyController {
 
+    @GetMapping("/current")
+    public String getCurrentEnergy() {
+        return "Mock Current Energy %";
+    }
+
+    @GetMapping("/historical")
+    public String getHistorical(@RequestParam String start, @RequestParam String end) {
+        return "Mock Historical Data from " + start + " to " + end;
+    }
 }
