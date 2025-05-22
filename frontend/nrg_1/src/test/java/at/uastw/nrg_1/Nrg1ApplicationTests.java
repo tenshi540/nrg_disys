1df316c01093a26a182189a38a7482ae8a42fb69
melozeroneuf <167721476+melozeroneuf@users.noreply.github.com>
2025-05-22 18:29:28 +0200

    changes

---
+++ frontend/nrg_1/src/test/java/at/uastw/nrg_1/Nrg1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.uastw.nrg_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nrg1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
